02c39b899889154f189f1c8ed138a14c276a21d2
mnaduk <maciej.naduk@gmail.com>
2016-02-24 18:12:38 +0100

    Create cwi.cpp

---
+++ mnaduk/cwi.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int x, y;
+cin >> x >> y;
+
+
+    if(x>0)
+    {
+    if(y>0)
+        cout << "I";
+    else if (y==0)
+        cout << "OX";
+    else
+        cout << "IV";
+    }
+    else if (x==0)
+    {
+        if (y==0)
+            cout << 0;
+        else
+            cout << "OY";
+    }
+    else
+    {
+        if(y>0)
+            cout << "II";
+        else if (y==0)
+            cout << "OX";
+        else
+            cout << "III";
+    }
+
+return 0;
+}
