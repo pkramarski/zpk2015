113fa43f8861e50afeec055bc1287110cfeaabcb
Karolina Orczykowska <karolinaorczykowska@gmail.com>
2016-02-24 18:15:44 +0100

    Create poc.cpp

---
+++ grupa2/korczykowska/poc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string name;
+  cout << "Mount Everest - 8850 m n.p.m.\n";
+  cout << "McKinley - 6194 m n.p.m.\n";
+  cout << "Mont Blanc - 4810 m n.p.m." << endl;
+
+  return 0;
+}
