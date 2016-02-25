38abc01db934f202bd91e9198d04de26913b91b2
lfilipiuk <lukasz.filipiuk@student.uw.edu.pl>
2016-02-25 16:31:33 +0100

    Update sto.cpp

--- lfilipiuk/p1/sto.cpp
+++ lfilipiuk/p1/sto.cpp
@@ -3,14 +3,34 @@ using namespace std;
 
 int main()
 {
-int A, B, K, krzesla;
+int A, B, K, krzesla_A=0, krzesla_B=0;
 
 cin >> A >> B >> K;
 
-B = B-2*K;
-krzesla=A/K;
-krzesla = krzesla+B/K;
-krzesla = 2*krzesla;
+//pierwsza sciana pionowa
+if(A>=K && B>=K)
+{
+krzesla_A=A/K;
+if(B-2*K>=0)
+{
+    krzesla_A=krzesla_A*2;
+    B=B-2*K;
+}
+else
+    B=B-K;
 
-cout << krzesla << endl;
+/*
+cout <<"na scianach A zmiesci sie: " << krzesla_A << endl;
+cout <<"sciana B ma teraz: " << B << endl;
+*/
+krzesla_B=B/K;
+
+if(A-2*K>=0)
+    krzesla_B=krzesla_B*2;
+/*
+cout <<"na scianach B zmiesci sie: " << krzesla_B << endl;
+*/
+}
+
+cout<<krzesla_A+krzesla_B<<endl;
 }
