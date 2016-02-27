659ca8a82f3097fac77dec96cef72ffcd0711e60
mzmudatrze <martyna.zmudatrzebiatowska@gmail.com>
2016-02-27 20:52:20 +0100

    Create max.cpp

---
+++ grupa3/mzmudatrze/max.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a;
+    int b;
+    
+    cin >> a >> b;
+    
+    if ( a > b)
+        cout << a << endl;
+    else 
+        cout << b << endl;
+}
+    
