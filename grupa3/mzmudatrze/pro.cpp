67b1ae74d081a1549137ed5c6af88068d008416f
mzmudatrze <martyna.zmudatrzebiatowska@gmail.com>
2016-02-27 20:00:20 +0100

    pro.cpp

---
+++ grupa3/mzmudatrze/pro.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a; 
+    int b;
+    int c;
+    
+    cin >> a >> b >>  c;
+    cout << a * b * c << endl;
+    cout << (2 * a * b) + (2 * b * c) + (2 * a * c) << endl;
+}
