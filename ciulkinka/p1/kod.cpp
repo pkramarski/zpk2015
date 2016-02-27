d751b7c83a7842444277cf4579a65a78c3ffe6c5
ciulkinka <kciulkin@student.uw.edu.pl>
2016-02-27 23:21:20 +0100

    Update kod.cpp

--- ciulkinka/p1/kod.cpp
+++ ciulkinka/p1/kod.cpp
@@ -9,11 +9,11 @@ int main() {
 
     if (kodB < 58) {
         kodB = kodB - 48;
-        cout << a * 16 + kodB << endl << (char)(a * 16 + kodB);
     } else if (kodB > 64) {
         kodB = 10 + kodB - 65;
-        cout << a * 16 + kodB << endl << (char)(a * 16 + kodB);
     }
-
+    
+    cout << a * 16 + kodB << endl << (char)(a * 16 + kodB);
+    
     return 0;
 }
