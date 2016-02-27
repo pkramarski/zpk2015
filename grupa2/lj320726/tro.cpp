4fe85071edc62d4b5724a1e7eeabf55c972927bd
lj320726 <ljablonski@student.uw.edu.pl>
2016-02-27 23:25:03 +0100

    Create tro.cpp

---
+++ grupa2/lj320726/tro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long int a,b,c;
+    cin >> a >> b >>c;
+    if( a+b > c && a+c>b && b+c>a)
+        cout << "TAK";
+    else 
+        cout << "NIE";
+}
