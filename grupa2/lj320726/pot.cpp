0e073a90470f70d849c68efa21e6c2c741fdb33e
lj320726 <ljablonski@student.uw.edu.pl>
2016-02-28 01:31:31 +0100

    Create pot.cpp

---
+++ grupa2/lj320726/pot.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//algorytm meeusa - znaleziony na stronie internetowej
+int main()
+{
+    int n,w;
+    cin >> n;
+    w=1;
+    while(w<=n)
+        {
+            cout <<w<<endl;
+            w=w*2;
+        }
+}
