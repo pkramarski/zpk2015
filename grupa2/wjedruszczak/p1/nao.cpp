ed2e809440fe3bad1c1049bf957ba5bfacb9658d
wjedruszczak <w.jedruszczak@student.uw.edu.pl>
2016-02-29 00:11:52 +0100

    Create nao.cpp

---
+++ grupa2/wjedruszczak/p1/nao.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// deklaracja zmiennych
+	int a, b, c;
+
+	// wczytywanie
+	cin >> a >> b >> c;
+
+	// wyswietlenie
+	cout << a << " " << b << " " << c << endl << c << " " << b << " " << a << endl;
+}
