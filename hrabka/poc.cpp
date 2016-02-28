b22846e321836eaf65e3c75da76402046c0c1338
Aleksandra Rabinska <aleksandra.rabinska@gmail.com>
2016-02-28 12:53:12 +0100

    Początek

---
+++ hrabka/poc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  std::string name;
+  std::cout << "Mount Everest - 8850 m n.p.m.\n";
+  std::cout << "McKinley - 6194 m n.p.m.\n";
+  std::cout << "Mont Blanc - 4810 m n.p.m.";
+  std::cout << std::endl;
+
+  return 0;
+}
