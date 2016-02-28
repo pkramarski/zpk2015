c4c803f10c4c1311bcaed3f155167f96c49a01f0
wjedruszczak <w.jedruszczak@student.uw.edu.pl>
2016-02-29 00:58:18 +0100

    Create lus.cpp

---
+++ grupa2/wjedruszczak/p1/lus.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// deklaracja zmiennych
+	unsigned int n;
+
+	// wczytywanie
+	cin >> n;
+
+	// usuniecie zer z konca
+	while (n % 10 == 0)
+		n /= 10;
+
+	// lustro
+	if (n / 10 == 0)
+		cout << n << endl;
+	else
+		while (n != 0) {
+			cout << n % 10;
+			n /= 10;
+		}
+	cout << endl;
+}
