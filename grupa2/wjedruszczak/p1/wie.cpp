be9db709f1ac24983f940c33e4d49d18c7aca040
wjedruszczak <w.jedruszczak@student.uw.edu.pl>
2016-02-29 01:11:10 +0100

    Update wie.cpp

--- grupa2/wjedruszczak/p1/wie.cpp
+++ grupa2/wjedruszczak/p1/wie.cpp
@@ -15,22 +15,27 @@ int main() {
 		m = 4;
 		cout << d << " " << m << endl;
 		return 0;
-	} else if (rok == 1954 || rok == 2049 || rok == 2106) {
+	}
+	else if (rok == 1954 || rok == 2049 || rok == 2106) {
 		// wyjatek II rodzaju
 		d = 18;
 		m = 4;
 		cout << d << " " << m << endl;
 		return 0;
-	} else if (rok >= 1800 && rok <= 1899) {
+	}
+	else if (rok >= 1800 && rok <= 1899) {
 		A = 23;
 		B = 4;
-	} else if (rok >= 1900 && rok <= 2099) {
+	}
+	else if (rok >= 1900 && rok <= 2099) {
 		A = 24;
 		B = 5;
-	} else if (rok >= 2100 && rok <= 2199) {
+	}
+	else if (rok >= 2100 && rok <= 2199) {
 		A = 24;
 		B = 6;
-	} else if (rok == 2200) {
+	}
+	else if (rok == 2200) {
 		A = 25;
 		B = 0;
 	}
@@ -46,7 +51,8 @@ int main() {
 		m = 3;
 		d = dg + eg + 22;
 		cout << d << " " << m << endl;
-	} else {
+	}
+	else {
 		m = 4;
 		d = dg + eg - 9;
 		cout << d << " " << m << endl;
