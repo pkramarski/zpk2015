2fdc983e34384b0dda1d85780cd6f50a8c987c0a
kdulna <kat.dulna@gmail.com>
2016-02-29 11:00:43 +0100

    Create max.cpp

---
+++ kdulna/p1/max.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    unsigned int a, b;
+    cin >> a >> b;
+    if (a >= b)
+        cout << a << endl;
+    else if (a < b)
+        cout << b << endl;
+}
