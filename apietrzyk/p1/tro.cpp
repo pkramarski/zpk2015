8193d7b8d12ec9b3b7fc46e64670ecff2f8184c3
apietrzyk <agnieszka.anna.pietrzyk@gmail.com>
2016-03-01 20:11:24 +0100

    Added files via upload

---
+++ apietrzyk/p1/tro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if ( b+c>a && a+c>b && a+b>c)
+        cout << "TAK" << endl;
+    else
+        cout << "NIE" << endl;
+return 0;
+}
