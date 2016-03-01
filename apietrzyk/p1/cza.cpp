ba478adfa9f4797fbff931bfb297c1dc06e44a8a
apietrzyk <agnieszka.anna.pietrzyk@gmail.com>
2016-03-01 19:52:38 +0100

    Added files via upload

---
+++ apietrzyk/p1/cza.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    cout << t/3600 << "g";
+    cout << (t-(t/3600)*3600)/60 << "m";
+    cout << (t-(t/3600)*3600)%60 << "s" << endl;
+return 0;
+}
+
+
