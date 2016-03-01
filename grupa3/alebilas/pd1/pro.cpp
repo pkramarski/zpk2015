82d6b891bc4f0c431fc0dc24f1e5d432173ad6d1
alebilas <bilas.aleksandra@gmail.com>
2016-03-01 12:54:46 +0100

    Create pro.cpp

---
+++ grupa3/alebilas/pd1/pro.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a * b * c << endl
+        << 2 * a * b + 2 * a * c + 2 * b * c << endl;
+}
