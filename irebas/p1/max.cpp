1f46a28cf263f84f219a6e5640e3dae8b6c1aa37
irebas <igor.rebas@gmail.com>
2016-03-01 19:30:52 +0100

    Create max.cpp

---
+++ irebas/p1/max.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    unsigned int a, b;
+cin >> a >> b;
+  
+  if (a > b)
+  	cout << a << endl;
+  else if (b > a)
+  	cout << b << endl;
+  else
+  	cout << "Liczby sa rowne" << endl;
+}
