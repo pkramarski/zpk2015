0d0fe7161797e3e971f78291b788a7ae00980460
DK362821 <d.kondratiuk@student.uw.edu.pl>
2016-03-01 14:47:10 +0100

    Create max.cpp

---
+++ grupa2/DK362821/max.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    unsigned long int a, b;
+      cin >> a >> b;
+        if (a>b)
+          cout << a << endl;
+        else
+          cout << b << endl;
+}
