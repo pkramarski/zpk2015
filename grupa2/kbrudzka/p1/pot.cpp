a4451ef439bbdd103bd46da289fde37e0b986f97
kbrudzka <kasiablaszczyk@interia.eu>
2016-03-01 22:56:52 +0100

    Create pot.cpp

---
+++ grupa2/kbrudzka/p1/pot.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    unsigned int n;
+    cin >> n;
+    int i=0;
+    int p=1;
+
+cout << endl;
+
+    while(i<=n){
+
+    cout << p << endl;
+    p*=2;
+    i=i+1;
+    }
+}
