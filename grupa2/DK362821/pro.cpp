56def10b8be0c11a448b969d3944ee676fa57685
DK362821 <d.kondratiuk@student.uw.edu.pl>
2016-03-01 14:18:55 +0100

    Create pro.cpp

---
+++ grupa2/DK362821/pro.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    unsigned long int a, b, c;
+      cin >> a >> b >> c;
+        cout << a * b * c << endl;
+        cout << 2 * a * b + 2 * a * c + 2 * b * c << endl;
+}
