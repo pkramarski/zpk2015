9b55c0535e1ba7d461525dde4a10b83a59ae1aef
kbrudzka <kasiablaszczyk@interia.eu>
2016-03-01 17:07:30 +0100

    Create nao.cpp

---
+++ grupa2/kbrudzka/p1/nao.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b, c;
+    cin >>  a >>  b >> c;
+    cout << endl;
+/* poczatkowo dodalam warunki dotyczace wczytywanych liczb ale w main2 pojawialy sie bledy mimo poprawnego dzialania programu; 
+dlatego ponizej - uproszczona wersja programu*/
+    cout << a << " " << b << " " << c << endl;
+    cout << c << " " << b << " " << a << endl;
+
+
+}
