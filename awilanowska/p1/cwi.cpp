73c48a44f6efa93bc0c87c39371c604a1e5f6835
awilanowska <annawilanowska@student.uw.edu.pl>
2016-03-01 11:39:32 +0100

    Create cwi.cpp

---
+++ awilanowska/p1/cwi.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    if (!(x == 0 || y==0)) {
+        if (x > 0 && y > 0)
+            cout << "I" << endl;
+        else if (x < 0 && y > 0)
+            cout << "II" << endl;
+        else if (x < 0 && y < 0)
+            cout << "III" << endl;
+        else
+            cout << "IV" << endl;
+    }
+    else {
+        if (x == 0 && y == 0)
+            cout << 0 << endl;
+        else if (y == 0)
+            cout << "OX" << endl;
+        else
+            cout << "OY" << endl;
+    }
+}
