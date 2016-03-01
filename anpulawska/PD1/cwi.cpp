f121b318b5514dc39b385c340698509ea7c98cc0
anpulawska <aniapulawska@o2.pl>
2016-03-01 22:59:01 +0100

    Create cwi.cpp

---
+++ anpulawska/PD1/cwi.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin >> x >> y;
+    if(x > 0 && y>0)
+        cout << "I" << endl;
+    else if (x < 0 && y > 0)
+      cout << "II" << endl;
+         else if (x < 0 && y < 0)
+            cout << "III" << endl;
+                else if (x > 0 && y < 0)
+                  cout << "IV" << endl;
+                    else if (x == 0 && y != 0)
+                       cout << "OY" << endl;
+                            else if (x != 0 &&  y== 0)
+                               cout << "OX" << endl;
+    else
+        cout << "0" << endl;
+}
