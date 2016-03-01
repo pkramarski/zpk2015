79f74f9885ce0ce2e0371820bf9d0f14e4fcca6c
kasiamalek <kasia.malek.11235@gmail.com>
2016-03-01 21:12:16 +0100

    Create kod.cpp

---
+++ grupa2/kasiamalek/z1/kod.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a;
+char b,c;
+cin>>b>>c;
+if(c>='A'&&c<='F')
+{
+cout<<(b-48)*16+c-55<<endl;
+cout<<(char)((b-48)*16+c-55)<<endl;
+}
+else
+{cout<<(b-48)*16+c-48<<endl;
+cout<<(char)((b-48)*16+c-48)<<endl;
+}
+}
