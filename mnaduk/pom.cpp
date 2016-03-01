d3fc22e7671bc3cfa06d2353d77d6ec019e9c62b
mnaduk <maciej.naduk@gmail.com>
2016-03-01 21:53:02 +0100

    Create pom.cpp

---
+++ mnaduk/pom.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	c=0;
+	b=0;
+	cin >> a;
+	while(b != -1)
+	{
+	cin >> b;
+	if(b>a)
+		c++;
+	a=b;	
+		
+	}
+	
+	cout << c;
+	
+return 0;	
+}
