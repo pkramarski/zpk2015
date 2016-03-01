4889fc3222cd4785502787aa37c8b5c137ea6dc6
DK362821 <d.kondratiuk@student.uw.edu.pl>
2016-03-01 14:02:05 +0100

    Update poc.cpp

--- grupa2/DK362821/poc.cpp
+++ grupa2/DK362821/poc.cpp
@@ -1,6 +1,9 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-    cout << "Pierwszy program!" << endl;
+int main()
+{
+    cout<<"Mount Everest - 8850 m n.p.m.\n"; /n dla przeniesienia wierszy/
+    cout<<"McKinley - 6194 m n.p.m.\n";
+    cout<<"Mont Blanc - 4810 m n.p.m.";
 }
