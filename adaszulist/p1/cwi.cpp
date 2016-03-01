77e3b5b9babefc2405cb4c78f6bcfe872b57668b
adaszulist <a.szulist@student.uw.edu.pl>
2016-03-01 12:11:20 +0100

    Create cwi.cpp

---
+++ adaszulist/p1/cwi.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    if (x == 0 &&  y != 0)
+        cout << "OY" << endl;
+    else if ( x != 0 && y == 0)
+        cout << "OX" << endl;
+    else if (x == 0 && y == 0)
+        cout << "0" << endl;
+    else if (x > 0 && y > 0)
+        cout << "I" << endl;
+    else if (x > 0 && y < 0)
+        cout << "II" << endl;
+    else if (x < 0 && y < 0)
+        cout << "III" << endl;
+    else
+        cout << "IV" << endl;
+return 0;
+}
