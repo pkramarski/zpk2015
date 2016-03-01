eb084c013901a42d775e3e5dd6854727711a911d
adaszulist <a.szulist@student.uw.edu.pl>
2016-03-01 12:10:10 +0100

    Create cza.cpp

---
+++ adaszulist/p1/cza.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t, g, m, s;
+    cin >> t;
+    g = t / 3600;
+    m = (t - g * 3600)/60;
+    s = t - g * 3600 - m * 60;
+    cout << g <<  "g" << m << "m" << s << "s" << endl;
+}
