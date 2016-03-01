eb7896981ccaf7e33f9c2d3def2abc5ee157377a
apietrzyk <agnieszka.anna.pietrzyk@gmail.com>
2016-03-01 20:57:03 +0100

    Added files via upload

---
+++ apietrzyk/p1/pot.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int n, j;
+    j=1;
+    cin >> n;
+    while (j<n) {
+        cout << j <<endl;
+       j=2*j;
+    }
+return 0;
+
+}
