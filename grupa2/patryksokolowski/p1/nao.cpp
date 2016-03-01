5d8b22c2a9163eed93fa95241973bc874bf3ef6f
patryksokolowski <patryk.sokolowski@student.uw.edu.pl>
2016-03-01 21:54:55 +0100

    Create nao.cpp

---
+++ grupa2/patryksokolowski/p1/nao.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int input[3];
+	cin >> input[0] >> input[1] >> input[2];
+	cout << input[0] << ' ' << input[1] << ' ' << input[2] << endl << input[2] << ' ' << input[1] << ' ' << input[0];
+}
