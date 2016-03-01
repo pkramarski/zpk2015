0d847f3c56bcf8ec3e0740dafc30ec2190b5bf80
irebas <igor.rebas@gmail.com>
2016-03-01 19:25:49 +0100

    Create pro.cpp

---
+++ irebas/p1/pro.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    unsigned int a,b,c;
+cin >> a >> b >> c;
+  cout << "Objętosc: " << a*b*c << endl;
+  cout << "Pole: " << 2*(a*b)+2*(a*c)+2*(b*c) << endl;
+}
