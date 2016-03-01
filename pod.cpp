12f07cb591ab156d21f7d89837948572dab9d053
gosia2702 <malgorzata.nachyla@student.uw.edu.pl>
2016-03-01 21:19:53 +0100

    Added files via upload

---
+++ pod.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+ using namespace std;
+
+ int main () {
+     int a,b,k,i,j;
+     j=0;
+     cin >> a >> b >>k;
+     for (i=a; i<b+1; i++) {
+         if(i%k==0)
+             j++;
+     }
+     cout << j;
+ }
