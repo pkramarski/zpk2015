d056accea134bebed29ef6bd2ee7788ac41b5bb9
Karolina Orczykowska <karolinaorczykowska@gmail.com>
2016-03-01 19:34:05 +0100

    Update sto.cpp

--- grupa2/korczykowska/p1/sto.cpp
+++ grupa2/korczykowska/p1/sto.cpp
@@ -5,11 +5,24 @@ int main() {
    int A, B, K;
       cin >> A >> B >> K;
     int krzesla; //liczba krzesel ktore postawi sie przy stole
-//1 bok: krzesla jest in wiec otrzymamy i tak liczbe calkowita
-    krzesla = A/K;
-//2 bok
-    krzesla = krzesla + (B - 2 * K)/K;
-//trzeba to pomnozyc przez 2, bo mamy po 2 boki takie same
-      cout << 2 * krzesla << endl;
 
+    if (A < K && B < K) //wogole nie wstawimy krzeseł
+        cout << 0 << endl;
+
+    else if (A > K && B > K && B < 2 * K && A < 2 * K) /*każdy bok zmiesci max 1 krzeslo - czyli sumarycznie uda sie wstawic tylko
+        1 krzeslo*/
+        cout << "1" << endl;
+
+    else if ((A > K && B > K && B < 2 * K) || (B > K && A > K && A < 2 * K))/*tutaj tylko mozemy ustawic krzesla po jednej stronie dluzszego boku*/
+        {krzesla = max(A, B)/K;
+        cout << krzesla << endl;}
+/*gdy oba boki > 2*K
+dzieki temu ze krzesla sa kwadratowe nie ma znaczenie, czy zaczniemy je ustawiac
+przy dluzszym boku, czy przy krotszym*/
+    else
+        {krzesla = A/K; /*pierwszy bok*/
+        krzesla = krzesla + (B - 2 * K)/K; /*drugi bok*/
+        krzesla = 2 * krzesla;
+/*trzeba to pomnozyc przez 2, bo mamy po 2 boki takie same*/
+        cout << krzesla << endl;}
 }
