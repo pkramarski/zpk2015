9dc8c1ec8f83b9aa29f993361188701d794a548c
kamprzadka <kamil.przadka1@gmail.com>
2016-03-02 17:02:32 +0100

    Update CWI.cpp

--- grupa2/kamprzadka/p1/CWI.cpp
+++ grupa2/kamprzadka/p1/CWI.cpp
@@ -7,8 +7,8 @@ int main()
   int x,y;
   cin>>x>>y;
   if(x==0 && y == 0) cout<<"0"<<endl; 
-  else if (x==0) cout<<"OX\n";
-  else if (y==0) cout<<"OY\n";
+  else if (x==0) cout<<"OY\n";
+  else if (y==0) cout<<"OX\n";
   else if (x>0 && y>0) cout<<"I\n";
   else if (x<0 && y>0) cout<<"II\n";
   else if (x<0 && y<0) cout<<"III\n";
