dfa605263608a43062df5979cbe052415f424a21
lfilipiuk <lukasz.filipiuk@student.uw.edu.pl>
2016-03-02 23:17:20 +0100

    Update dwa.cpp

--- grupa1/lfilipiuk/p2/dwa.cpp
+++ grupa1/lfilipiuk/p2/dwa.cpp
@@ -4,21 +4,22 @@
 using namespace std;
 
 int main(){
-int n, m, suma, suma_min;
+int n, m, suma, suma_min=1000000;
 
 cin >> n >> m;
 
 int t[n][m];
 
 for(int i=0;i<n;i++)
-    for(int j=0;j<m;j++)
-        cin >> t[i][j];
+    for(int k=0;k<m;k++)
+        cin >> t[i][k];
 
 for(int i=0;i<n-1;i++)
-    for(int j=i+1;j<n;j++){
+    for(int j=i;j<n;j++){
         for(int k=0;k<m;k++)
             suma=suma+min(t[i][k],t[j][k]);
         suma_min = min(suma, suma_min);
+        suma=0;
         }
 
 cout<<suma_min<<endl;
