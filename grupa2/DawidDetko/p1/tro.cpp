129edb72221edc77731b507521f068444c3beb43
dawiddetko <dawiddetko@gmail.com>
2016-03-03 10:11:40 +0100

    TROJKAT

---
+++ grupa2/DawidDetko/p1/tro.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+ unsigned int a,b,c;
+ cin >> a >> b >> c;
+ if (a + b > c && a + c > b && b + c > a)
+ {
+ cout << "TAK";
+ 
+ }
+ else 
+ {
+  cout << "NIE";
+ }
+}
+ 
