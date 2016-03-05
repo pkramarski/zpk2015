ba0821794fddadace51cc5275a8b3257841868a0
Karolina Orczykowska <karolinaorczykowska@gmail.com>
2016-03-05 19:46:04 +0100

    Create sil.cpp

---
+++ grupa2/korczykowska/p2/sil.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int n;
+      cin >> n;
+    int silnia = 1;
+    if (n == 0)
+        cout << silnia <<endl;
+    else {
+        for (int i = 1; i <= n; i++){
+             silnia = silnia * i;
+        }
+        cout << silnia <<endl;
+        }
+}
