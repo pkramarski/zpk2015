97f4ac01d31aa235fab3143346617c2f44daf06b
Karolina Orczykowska <karolinaorczykowska@gmail.com>
2016-03-05 14:00:42 +0100

    Create czy.cpp

---
+++ grupa2/korczykowska/p2/czy.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int i;
+      cin >> i;
+
+    while (i % 2 == 0){
+                i = i/2;}
+        if (i == 1)
+            cout << "TAK" << endl;
+        else
+            cout <<  "NIE" << endl;
+
+    }
