145b2f1e111c37a570bd0f90eb8d7dca10c63eed
mnaduk <maciej.naduk@gmail.com>
2016-03-07 23:38:17 +0100

    Create kol.cpp

---
+++ grupa1/mnaduk/p2/kol.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+double r;
+cin >> r;
+cout << setprecision(3) << fixed << M_PI * r * r << endl << 2*M_PI*r;	
+	
+	
+	
+return 0;	
+}
