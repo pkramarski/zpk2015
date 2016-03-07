bdb1ed6847a9bb97240f3a2c8ac65d7e7cc4ce6f
Edi <e.lasek@student.uw.edu.pl>
2016-03-07 21:05:39 +0100

    pro.cpp

---
+++ grupa1/koredmat/p1/pro.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a;
+    int b;
+    int c;
+    cin >> a >> b >> c;
+    cout << a*b*c << endl;
+    cout << 2*(a*b+a*c+b*c) << endl;
+}
