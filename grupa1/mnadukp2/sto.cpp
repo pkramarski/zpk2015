0806abba3c331ff17d884b951560e07b2a02b38d
mnaduk <maciej.naduk@gmail.com>
2016-03-08 00:06:02 +0100

    Create sto.cpp

---
+++ grupa1/mnadukp2/sto.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int A, B, K, poz, pio;
+cin >> A >> B >> K;
+poz=A/K;
+pio=B/K;
+if (pio==0 || poz==0)
+    cout << 0;
+else if (poz==1)
+    cout << pio;
+else if (pio==1)
+    cout << poz;
+else
+    cout << pio*2 + 2*(poz-2);
+
+
+return 0;
+}
