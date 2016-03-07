7b07ea3ebc84abdd30fedbf386c49ea0df38a400
kkokoszka <karol.kokoszka@student.uw.edu.pl>
2016-03-07 22:16:00 +0100

    Added files via upload

---
+++ grupa2/kkokoszka/PD2/nap.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, i;
+    cin>>n;
+    int t[n];
+    for(i=0; i<n; i++){
+        cin>> t[i];
+    }
+    for(i=0; i<n; i=i+2){
+        cout<< t[i]<< " ";
+    }
+    cout<<endl;
+    for(i=1; i<n; i=i+2){
+        cout<< t[i]<< " ";
+    }
+
+}
+
