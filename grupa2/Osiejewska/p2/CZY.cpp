770bcbef2230109bbf75d4731fa450a0cb990d1f
Osiejewska <paulina.osiejewska@gmail.com>
2016-03-08 22:42:21 +0100

    Create CZY.cpp

---
+++ grupa2/Osiejewska/p2/CZY.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+long long i;
+cin >> i;
+
+    while (i != 1 && i % 2 == 0)
+        i = i / 2;
+
+    if (i == 1)
+        cout << "TAK" << endl;
+    else
+        cout << "NIE" << endl;
+}
