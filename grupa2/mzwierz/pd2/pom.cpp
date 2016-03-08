cb1ccfc2b61ad9c8597fcc945dd834cc6c238cec
mzwierz <mzwierz@student.uw.edu.pl>
2016-03-08 23:54:50 +0100

    Added files via upload

---
+++ grupa2/mzwierz/pd2/pom.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int n=0;
+int pomiar=0;
+int i=1;
+while(i>=0){
+
+    cin>>i;
+    if (pomiar<i){
+            n++;
+    pomiar=i;
+
+    }
+}
+cout<<n;
+
+}
---
+++ grupa2/mzwierz/pd2/pot.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int n;
+cin>>n;
+int wynik=1;
+cout<<wynik<<endl;
+while(wynik<n){
+    		wynik=wynik*2;
+    		if(wynik<=n){
+        			cout<<wynik<<endl;
+				}
+		}
+}
