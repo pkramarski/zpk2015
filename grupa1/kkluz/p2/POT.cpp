f7f2398a4bf321286c057d223ba05e4f09747da0
kkluz <katarzyna_kluz@outlook.com>
2016-03-08 15:21:00 +0100

    Create POT.cpp

---
+++ grupa1/kkluz/p2/POT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int potega, n;
+	cin >> n;
+	potega=1;
+	while(potega <=n )
+	{
+	cout << potega << endl;
+	potega=potega*2;	
+	}
+	
+return 0;	
+}
