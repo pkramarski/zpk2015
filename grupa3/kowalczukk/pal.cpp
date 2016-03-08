e5b3431c18ca42f5ff4b05cea4053171f5da86a8
kowalczukk <kowal012@gmail.com>
2016-03-08 21:40:09 +0100

    Create pal.cpp

---
+++ grupa3/kowalczukk/pal.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string x;
+    cin >> x;
+    bool pal = true;
+    n = x.size()
+    for (int i = 0; i < n; i++) {
+        if (pal[i] != pal[n-1-i])
+            pal = false;
+    }
+    if (pal)
+        cout << "TAK" << endl;
+    else
+        cout << "NIE" << endl;
+}
