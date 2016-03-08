c5d69d701819d19ef6884411801241800e5edb27
Osiejewska <paulina.osiejewska@gmail.com>
2016-03-08 20:48:41 +0100

    Create KOL.cpp

---
+++ grupa2/Osiejewska/p2/KOL.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main(){
+double r;
+cin >> r;
+
+cout << setprecision(3) << fixed;
+cout << r*r*M_PI << endl;
+cout << 2*r*M_PI << endl;
+}
