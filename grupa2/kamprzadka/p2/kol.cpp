c0aa9d4c9deb3c621e8252debb8d34674b078b62
kamprzadka <kamil.przadka1@gmail.com>
2016-03-10 22:34:02 +0100

    Create kol.cpp

---
+++ grupa2/kamprzadka/p2/kol.cpp
@@ -0,0 +1,17 @@
+//kol
+#include<iostream>
+#include<math.h>
+#include <iomanip>
+using namespace std;
+
+
+int main()
+{
+    double r;
+    cin>>r;
+
+    cout<<fixed<<setprecision(3)<< M_PI*r*r<<endl;
+    cout<<fixed<<setprecision(3)<< 2*M_PI*r<<endl;
+
+    return 0;
+}
