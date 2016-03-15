43c936e400218f7891a600317f5668254ccc070b
patryksokolowski <patryk.sokolowski@student.uw.edu.pl>
2016-03-15 21:36:52 +0100

    Create sil.cpp

---
+++ grupa2/patryksokolowski/p2/sil.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+int n;
+int i = 1;
+int sil = 1;
+    cin >> n;
+
+    for (i=1;i<=n;i++) {
+     sil = sil * i ;
+    }
+cout << sil;
+}
