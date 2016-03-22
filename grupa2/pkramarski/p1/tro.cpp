21c076a47f81d68841db30ac54518aea6a1c2d47
pkramarski <pawel.kramarski@student.uw.edu.pl>
2016-03-22 21:16:55 +0100

    Create tro.cpp

---
+++ grupa2/pkramarski/p1/tro.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+    if(a>0 && b>0 && c>0 && a<b+c && b<a+c && c<a+b)
+        cout << "TAK";
+    else
+        cout << "NIE";
+}
