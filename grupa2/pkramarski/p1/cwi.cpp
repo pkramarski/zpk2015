073a5e76c4f65b47b10994085f77d7ac306d0be5
pkramarski <pawel.kramarski@student.uw.edu.pl>
2016-03-22 21:16:27 +0100

    Create cwi.cpp

---
+++ grupa2/pkramarski/p1/cwi.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+    if(x>0 && y>0)
+        cout << "I";
+    if(x<0 && y>0)
+        cout << "II";
+    if(x<0 && y<0)
+        cout << "III";
+    if(x>0 && y<0)
+        cout << "IV";
+    if(x==0 && y==0)
+        cout << "0";
+    if(x==0 && y!=0)
+        cout << "OY";
+    if(x!=0 && y==0)
+        cout << "OX";
+}
