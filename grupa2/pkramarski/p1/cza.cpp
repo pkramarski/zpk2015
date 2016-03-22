2a5ac969f5963007d9a5a34c96a58a38436616ff
pkramarski <pawel.kramarski@student.uw.edu.pl>
2016-03-22 21:15:20 +0100

    Create cza.cpp

---
+++ grupa2/pkramarski/p1/cza.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int a, g, m, s;
+
+    cin >> a;
+    g = a/3600;
+    m = (a - g*3600)/60;
+    s = (a - g*3600 - m*60);
+    cout << g << "g" << m << "m" << s << "s" << endl;
+}
